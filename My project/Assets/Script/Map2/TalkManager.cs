1e17e346f10ff959b501b03c14e8bfe502d907d2
Yj621 <87031282+Yj621@users.noreply.github.com>
2023-03-16 00:51:25 +0900

    NPC Commit Test
    NPC Commit Test

--- My project/Assets/Script/Map2/TalkManager.cs
+++ My project/Assets/Script/Map2/TalkManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TalkManager : MonoBehaviour
 {
     Dictionary<int, string[]> talkData; //키,값
+
     void Awake()
     {
         talkData= new Dictionary<int, string[]>();
@@ -13,6 +14,11 @@ public class TalkManager : MonoBehaviour
 
     void GenerateData()
     {
-        talkData.Add(1000,);
+        talkData.Add(1000,new string[] {"안녕?", "여긴 어쩐일이야?", "외부인이 온 것도 오랜만이네.", "아, 나는 여기 주민이야", "참 깨끗한 마을이었지..", "지금도 좋아보인다고?",
+        "예전에는.. 호수가 이러지 않았는데 말이야.", "호수가 어디있냐고?", "문 밖으로 나가면 있어."});
+    }
+    public string GetTalk(int id, int talkIndex)
+    {
+        return talkData[id][talkIndex];
     }
 }
