124a1a224e858fe00aa4e3121fc75890d21bfff2
YangWooSong <olivia6844@naver.com>
2023-03-19 17:18:46 +0900

    IdontHavekeyGImage

--- My project/Assets/Script/Map1/Map1.cs
+++ My project/Assets/Script/Map1/Map1.cs
@@ -6,28 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class Map1_2 : MonoBehaviour
 {
+    private GameObject Map2_icon;
     // Start is called before the first frame update
     void Start()
     {
-
+        Map2_icon = GameObject.Find("Map2Icon");
+        Map2_icon.SetActive(false);
     }
         // Update is called once per frame
     void Update()
     {
 
     }
-
-
     private void OnTriggerStay2D(Collider2D collision) //씬이동
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+             Map2_icon.SetActive(true);
             if (Input.GetKey(KeyCode.G))//GetKey사용하면 누를때 바로 이동됨
-            {
-                
+            {  
                 SceneManager.LoadScene(4);
             }
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Map2_icon.SetActive(false);
     }
 }
 
--- My project/Assets/Script/Map1/Map1_2.cs
+++ My project/Assets/Script/Map1/Map1_2.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class Map1 : MonoBehaviour
 {
+    private GameObject H_icon;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        H_icon = GameObject.Find("HouseIcon");
+        H_icon.SetActive(false);
     }
         // Update is called once per frame
     void Update()
@@ -22,12 +25,16 @@ public class Map1 : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            H_icon.SetActive(true);
             if (Input.GetKey(KeyCode.G))//GetKey사용하면 누를때 바로 이동됨
-            {
-                
+            { 
                 SceneManager.LoadScene(2);
             }
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision) 
+    {
+        H_icon.SetActive(false);
     }
 }
 
